7b44025b9fb3d8b82aa0626039876c64ec27e408
sabinAnwar <sabin.elanwar@iu-study.org>
2026-07-19 15:51:42 +0200

    add styling and functionality for grocery list app

--- Grocery_bud/app.js
+++ Grocery_bud/app.js
@@ -4,6 +4,7 @@ const form = document.querySelector(".grocery-form");
 const grocery = document.getElementById("grocery");
 const submitBtn = document.querySelector(".submit-btn");
 const list = document.querySelector(".grocery-list");
+const container = document.querySelector(".grocery-container");
 const clearBtn = document.querySelector(".clear-btn");
 
 // edit option
@@ -20,15 +21,48 @@ form.addEventListener("submit", addItem);
 function addItem(e) {
   e.preventDefault();
   const value = grocery.value;
-  if (value) {
+
+  if (!value) {
     console.log("value is falsy");
   }
   const id = new Date().getTime().toString();
-  if (value !== '' && !editFlag) {
-    console.log("adding item to the list");
-  } else if (value !== '' && editFlag === true) {
+  if (value && !editFlag) {
+    const element = document.createElement("article");
+    // add class
+    element.classList.add("grocery-item");
+    // add id
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<p class="title">${value}</p><div class="btn-container">
+    <button class="edit-btn" type="button">
+    <i class="fas fa-edit"></i>
+    </button>
+    <button class="delete-btn" type="button">
+    <i class="fas fa-trash"></i>
+    </button>
+    </div>`;
+    //   append Child
+    list.appendChild(element);
+    // display alert
+    displayAlert("item added to the list", "success");
+    container.classList.add("show-container");
+  } else if (value && editFlag) {
     console.log("editing");
   } else {
-    alert.textContent = "please enter value";
+    displayAlert("please enter a value", "danger");
   }
 }
+//display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+
+  // remove alert
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
+
+//****** Local Storage *******/
